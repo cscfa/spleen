148e632b1b87b9a608d570a3020cf81a672040a8
matthieu88160 <matthieu.vallance@cscfa.fr>
2015-06-28 01:14:00 +0200

    fix import

--- src/org/spleen/JUnit/CacheContainerTest.java
+++ src/org/spleen/JUnit/CacheContainerTest.java
@@ -1,9 +1,13 @@
 package org.spleen.JUnit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
-import java.util.Set;
 
 import org.junit.Test;
 import org.spleen.collection.NamespaceMultiThread;
